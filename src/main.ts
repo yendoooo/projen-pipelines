6f8530eb78c230adea2ffeae472528f4a26f3484
yendoooo <yendoooo1004@gmail.com>
2025-06-29 23:45:27 +0900

    feat: create the iam role for pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,48 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, CfnOutput, RemovalPolicies, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const githubRepository = 'yendoooo/projen-pipelines';  // 自身の環境に合わせて修正
+
+    const githubProvider = new iam.OpenIdConnectProvider(this, 'GitHubProvider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIds: ['sts.amazonaws.com'],
+      thumbprints: ['6938fd4d98bab03faadb97b34396831e3780aea1'],
+    });
+
+    const githubActionsPolicy = new iam.ManagedPolicy(this, 'GitHubActionsPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['*'],
+          resources: ['*'],
+        }),
+      ],
+    });
+
+    const githubActionsRole = new iam.Role(this, 'GitHubActionsRole', {
+      assumedBy: new iam.FederatedPrincipal(
+        githubProvider.openIdConnectProviderArn,
+        {
+          "StringEquals": {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+          },
+          "StringLike": {
+            "token.actions.githubusercontent.com:sub": `repo:${githubRepository}:*`
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity",
+      ),
+      managedPolicies: [githubActionsPolicy],
+    });
+
+    new CfnOutput(this, 'GitHubActionsRoleArn', {
+      value: githubActionsRole.roleArn,
+      exportName: 'GitHubActionsRoleArn',
+    });
   }
 }
 
@@ -17,7 +54,8 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, 'projen-pipelines-dev', { env: devEnv });
+const myStack = new MyStack(app, 'MyStack', { env: devEnv });
+RemovalPolicies.of(myStack).destroy()
 // new MyStack(app, 'projen-pipelines-prod', { env: prodEnv });
 
 app.synth();
