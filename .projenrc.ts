852b757931000e2e3437db3fae6be8d20bab325c
yendoooo <yendoooo1004@gmail.com>
2025-06-29 22:20:36 +0900

    chore: install projen-pipelines

--- .projenrc.ts
+++ .projenrc.ts
@@ -5,9 +5,6 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   name: 'projen-pipelines',
   projenrcTs: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  devDeps: ['projen-pipelines'],  // 追加
 });
 project.synth();
