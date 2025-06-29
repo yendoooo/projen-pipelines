2dcfd87944d41bfe5f06f521bdc5c1723d686ec7
yendoooo <yendoooo1004@gmail.com>
2025-06-30 00:09:49 +0900

    feat: create the pipeline

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,6 @@
 import { awscdk } from 'projen';
+import { GithubCDKPipeline } from 'projen-pipelines';
+
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
@@ -7,4 +9,17 @@ const project = new awscdk.AwsCdkTypeScriptApp({
 
   devDeps: ['projen-pipelines'],  // 追加
 });
+
+new GithubCDKPipeline(project, {
+  iamRoleArns: {
+    default: 'arn:aws:iam::905418098783:role/MyStack-GitHubActionsRole4F1BBA26-0oo0O6Q4F8nq',
+  },
+  stages: [
+    {
+      name: 'dev',
+      env: { account: '905418098783', region: 'ap-northeast-1' },
+    },
+  ],
+});
+
 project.synth();
